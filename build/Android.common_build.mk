77c42e42c4193cb000f95c0b884973f224e42911
Dave Kessler <activethrasher00@gmail.com>
2015-02-22 03:12:28 -0500

    [temp] add an empty text file to /art
    Change-Id: Ia0879208a0419b3fee77343f048b97a03069380b

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -95,6 +95,7 @@ ifeq ($(WITH_ART_USE_OPTIMIZING_COMPILER), true)
 ART_USE_OPTIMIZING_COMPILER := true
 endif
 
+# Lets just do a little experiment here...
 ifeq ($(ART_USE_OPTIMIZING_COMPILER),true)
 DEX2OAT_FLAGS := --compiler-backend=Optimizing
 DALVIKVM_FLAGS += -Xcompiler-option --compiler-backend=Optimizing
